6d7ba396a547cfd1926b2a178b159c92e1410ea8
irfanAzmi1999 <eesyrafeerfan90@gmail.com>
2023-11-22 14:32:19 +0800

    add table attendances

---
+++ database/migrations/2023_11_22_061833_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->date('dateRecord')->nullable();
+            $table->decimal('latitudde')->nullable();
+            $table->decimal('longitude')->nullable();
+            $table->text('remarks');
+            $table->bigInteger('staff_id')->unsigned();
+            $table->foreign('staff_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
