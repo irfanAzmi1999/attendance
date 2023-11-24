b9c7a1eb8f6de6ae2740c298e7f025ee9ce55ecb
irfanAzmi1999 <eesyrafeerfan90@gmail.com>
2023-11-24 10:21:17 +0800

    add logo remove back button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -121,11 +121,12 @@
         <!-- Header Content -->
         <div class="header-content position-relative d-flex align-items-center justify-content-between">
             <!-- Back Button -->
-            <div class="back-button"><a href="elements.html">
-                    <svg class="bi bi-arrow-left-short" width="32" height="32" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"></path>
-                    </svg></a></div>
+{{--            <div class="back-button"><a href="elements.html">--}}
+{{--                    <svg class="bi bi-arrow-left-short" width="32" height="32" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">--}}
+{{--                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"></path>--}}
+{{--                    </svg></a></div>--}}
             <!-- Page Title -->
+            <img src="assets/localimage/MANTA_high_logo.png" width="50" alt="">
             <div class="page-heading">
                 <h6 class="mb-0">Attendance</h6>
             </div>
